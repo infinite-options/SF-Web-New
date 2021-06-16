2c36bc846c9d1dd61b3ef0b6535ff6ce893c6a22
Donovanphenry <=>
2021-06-16 12:24:43 -0700

    ProfileInfo lint

--- src/components/Profile-Info/ProfileInfo.js
+++ src/components/Profile-Info/ProfileInfo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, {  useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { AuthContext } from 'auth/AuthContext';
@@ -105,7 +105,6 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
     width: '75%',
     height: '50px',
-    color: 'primary',
     background: '#e88330',
     color: 'white',
     borderRadius: '10px',
@@ -162,17 +161,17 @@ function ProfileInfo() {
     ),
     GOOGLE: (
       <Box className={classes.socialSigninWrapper}>
-        <img height="60px" src={GoogleSignin} />
+        <img height="60px" src={GoogleSignin} alt = {''} />
       </Box>
     ),
     FACEBOOK: (
       <Box className={classes.socialSigninWrapper}>
-        <img height="60px" src={FacebookSignin} />
+        <img height="60px" src={FacebookSignin} alt = {''} />
       </Box>
     ),
     APPLE: (
       <Box className={classes.socialSigninWrapper}>
-        <img height="60px" src={AppleSignin} />
+        <img height="60px" src={AppleSignin} alt = {''} />
       </Box>
     ),
   };
@@ -238,7 +237,7 @@ function ProfileInfo() {
       };
       setProfile(updatedProfile);
     }
-  }, []);
+  }, [Auth.isAuth, history, setProfile]);
 
   const handleClickLogOut = () => {
     localStorage.removeItem('currentStorePage');
@@ -255,7 +254,7 @@ function ProfileInfo() {
   const onSubmit = (event) => {
     const AuthMethods = new AuthUtils();
 
-    if (newPassword != '' && newPassword == confirmedPassword) {
+    if (newPassword !== '' && newPassword === confirmedPassword) {
       const cred = {
         customer_email: profile.email,
         password: newPassword,
@@ -332,20 +331,20 @@ function ProfileInfo() {
           <TextField
             className={classes.profileEditField}
             variant="outlined"
-            label={profile.firstName == '' ? 'John' : profile.firstName}
+            label={profile.firstName === '' ? 'John' : profile.firstName}
             disabled
           />
           <TextField
             className={classes.profileEditField}
             variant="outlined"
-            label={profile.lastName == '' ? 'Doe' : profile.lastName}
+            label={profile.lastName === '' ? 'Doe' : profile.lastName}
             disabled
           />
           <TextField
             className={classes.profileEditField}
             variant="outlined"
             placeholder={
-              profile.phoneNum == '' ? '123-456-7891' : profile.phoneNum
+              profile.phoneNum === '' ? '123-456-7891' : profile.phoneNum
             }
             onChange={(event) =>
               setProfile({ ...profile, phoneNum: event.target.value })
@@ -354,13 +353,13 @@ function ProfileInfo() {
           <TextField
             className={classes.profileEditField}
             variant="outlined"
-            label={profile.email == '' ? 'johndoe@example.com' : profile.email}
+            label={profile.email === '' ? 'johndoe@example.com' : profile.email}
             disabled
           />
 
           {Auth.isAuth ? loginTypeMapping[profile.socialMedia] : ''}
 
-          <Box hidden={profile.socialMedia != 'NULL' || !resetPasswordClicked}>
+          <Box hidden={profile.socialMedia !== 'NULL' || !resetPasswordClicked}>
             <TextField
               className={classes.profileEditField}
               variant="outlined"
@@ -382,7 +381,7 @@ function ProfileInfo() {
             className={classes.profileEditField}
             variant="outlined"
             placeholder={
-              profile.address == '' ? 'Street Address' : profile.address
+              profile.address === '' ? 'Street Address' : profile.address
             }
             onChange={(event) =>
               setProfile({ ...profile, address: event.target.value })
@@ -391,7 +390,7 @@ function ProfileInfo() {
           <TextField
             className={classes.profileEditField}
             variant="outlined"
-            placeholder={profile.unit == '' ? 'Appt number' : profile.unit}
+            placeholder={profile.unit === '' ? 'Appt number' : profile.unit}
             onChange={(event) =>
               setProfile({ ...profile, unit: event.target.value })
             }
@@ -401,7 +400,7 @@ function ProfileInfo() {
             <TextField
               className={classes.profileEditField}
               variant="outlined"
-              placeholder={profile.city == '' ? 'City' : profile.city}
+              placeholder={profile.city === '' ? 'City' : profile.city}
               style={{ marginRight: '30px' }}
               onChange={(event) =>
                 setProfile({ ...profile, city: event.target.value })
@@ -410,7 +409,7 @@ function ProfileInfo() {
             <TextField
               className={classes.profileEditField}
               variant="outlined"
-              placeholder={profile.state == '' ? 'State' : profile.state}
+              placeholder={profile.state === '' ? 'State' : profile.state}
               style={{ marginRight: '30px' }}
               onChange={(event) =>
                 setProfile({ ...profile, state: event.target.value })
@@ -419,7 +418,7 @@ function ProfileInfo() {
             <TextField
               className={classes.profileEditField}
               variant="outlined"
-              placeholder={profile.zip == '' ? 'Zip Code' : profile.zip}
+              placeholder={profile.zip === '' ? 'Zip Code' : profile.zip}
               onChange={(event) =>
                 setProfile({ ...profile, zip: event.target.value })
               }
